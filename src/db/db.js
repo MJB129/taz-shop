6dca45c8a9811c71f41d1cb85801859f990c1f3b
Marcus Butler <123020346+MJB129@users.noreply.github.com>
2023-12-13 13:27:50 -0500

    Beta (#2)
    * added new font 'chronic2001'
    
    * changed font and txt color
    
    * made some visual changes.
    
    * added gifs and jpg files to the cards
    
    * changed the gif sizes

--- src/db/db.js
+++ src/db/db.js
@@ -1,17 +1,16 @@
-import accessoriesImg from "../images/accessories.jpg";
-import concentratesImg from "../images/concentrates.jpeg";
-import ediblesImg from "../images/edibles.jpg";
-import flowertestpicImg from "../images/flowertestpic.jpg";
-import shakeImg from "../images/shake.jpg";
-import shroomsImg from "../images/shrooms.jpg";
-import smallsImg from "../images/smalls.jpg";
-import burrprintbgImg from "../images/burrprint-bg.png";
+import accessoriesImg from "../images/weed-accessories.jpg";
+import concentratesImg from "../images/asst-concentrates.jpg";
+import ediblesImg from "../images/asst-edibles.jpg";
+import shroomsImg from "../images/shroom-ropes.jpg";
+import backpocketImg from "../images/back-pocket.gif";
+import frontpocketImg from "../images/front-pocket.gif";
+import fifthpocketImg from "../images/fifth-pocket.gif";
 
 export function getData() {
   return [
-    { title: "Fifth Pocket", price: 75, Image: flowertestpicImg,id:1 },
-    { title: "Back Pocket", price: 40, Image: smallsImg,id:2 },
-    { title: "Front Pocket", price: 25, Image: shakeImg ,id:3},
+    { title: "Fifth Pocket", price: 75, Image: fifthpocketImg,id:1 },
+    { title: "Back Pocket", price: 40, Image: backpocketImg,id:2 },
+    { title: "Front Pocket", price: 25, Image: frontpocketImg ,id:3},
     { title: "Accessories", price: 5, Image: accessoriesImg,id:4 },
     { title: "Concentrates", price: 40, Image: concentratesImg,id:5 },
     { title: "Shrooms", price: 30, Image: shroomsImg,id:6 },
