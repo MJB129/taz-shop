af525a3278641e2418c1d006125f09c625a5c2a3
MJB129 <romedigital2.0@gmail.com>
2023-12-13 13:09:22 -0500

    added gifs and jpg files to the cards

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./Components/Card/Card";
 import Cart from "./Components/Cart/Cart";
-import burrprintbg from "./images/burrprint-bg.png";
+
 const { getData } = require("./db/db");
 const buds = getData();
 
